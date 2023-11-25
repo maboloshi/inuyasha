53f8c5bf087ce7959411eee7c657d7edd471c9e6
沙漠之子 <7850715+maboloshi@users.noreply.github.com>
2023-11-25 22:05:03 +0800

    Update ci.sh

--- script/ci.sh
+++ script/ci.sh
@@ -2,8 +2,8 @@
 # echo $1 $2 $3 $4
 TOKEN=$1
 # repoNwo=$2
-remote=`git remote |head -1`
-repoNwo=`git remote get-url $remote | sed 's/.*://;s/\.git//'`
+remote=$(git remote | head -1)
+repoNwo=$(git remote get-url $remote | sed 's/.*://;s/\.git//')
 branch=$(git rev-parse --abbrev-ref HEAD) # 或 git rev-parse HEAD
 file_path=$2
 encoded_file_content=$(base64 < "$file_path")
@@ -12,11 +12,7 @@ message_body="Signed-off-by: 沙漠之子 <7850715+maboloshi@users.noreply.githu
 expectedHeadOid=$(git rev-parse HEAD)
 
 curl $GITHUB_GRAPHQL_URL --silent \
-     # GITHUB_GRAPHQL_URL= https://api.github.com/graphql
-     # echo HTTP status to demonstrate how GraphQL
-     # uses HTTP a mere RPC transport, response is 200 OK
-     # no matter what errors happen.
-     --write-out '%{stderr}HTTP status: %{response_code}\n\n'
+     --write-out '%{stderr}HTTP status: %{response_code}\n\n' \
      -H "Authorization: bearer $TOKEN" \
      --data @- <<GRAPHQL | jq
 {
@@ -34,7 +30,7 @@ curl $GITHUB_GRAPHQL_URL --silent \
         "branchName": "$branch"
       },
       "message": {
-        "headline": "$message_headline"
+        "headline": "$message_headline",
         "body": "$message_body"
       },
       "fileChanges": {
